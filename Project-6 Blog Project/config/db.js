f54e71c87844c922e67153120a812adcb119d294
sonaljinjala1 <sonaljinjala1@gmail.com>
2025-02-14 07:23:51 +0530

    Blog Project

--- Project-6 Blog Project/config/db.js
+++ Project-6 Blog Project/config/db.js
@@ -4,13 +4,13 @@ mongoose.connect('mongodb://localhost/login-with-cookie-blog');
 
 const db = mongoose.connection;
 
-db.on("connected",(err) => {
-    if(err){
+db.on("connected", (err) => {
+    if (err) {
         console.log(err);
         return false;
     }
     console.log("Database connected successfully......!");
-    
+
 })
 
 module.exports = db;
